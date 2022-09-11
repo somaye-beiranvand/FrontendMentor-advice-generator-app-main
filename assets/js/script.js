207a6c546355a8052389b7e499e50ee390be14b8
unknown <beiranvand.somaye@gmail.com>
2022-09-11 23:56:37 +0430

    add chache:false to fix mozila  problem at going next advice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   const id = $(".card__title span");
   const advice = $(".card__text");
-  $(".card__btn").on("click", function () {
+  const btn = $(".card__btn");
+  $.ajaxSetup({
+    cache: false,
+  });
+  btn.on("click", function () {
     $.getJSON("https://api.adviceslip.com/advice", function (data) {
       var items = [];
       $.each(data, function (key, val) {
